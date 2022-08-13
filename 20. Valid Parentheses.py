87c7b0b3023caef67870deb2d63de0c698f8be33
Davicheanin Meas <42645102+MeasDavicheanin@users.noreply.github.com>
2022-08-13 10:51:04 -0600

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,7 +1,37 @@
-s = "()"
+class Stack:
+    def __init__(self) -> None:
+        self.items=[]
+    
+    def isempty(self):
+        return self.items==[]
+    
+    def push(self, item):
+        self.items.insert(0,item)
+    
+    def pop(self):
+        self.items.pop(0)
 
-s = "("[::-1]
+    def size(self):
+        return len(self.items)
+    def peek(self):
+        return self.items[0]
 
-print(s)
-
-stack=[]
+class Solution:
+    def isValid(self, s: str) -> bool:
+        sta=Stack()
+        ClosetoOpenMap={
+            ")":"(", "}":"{","]":"["
+        }
+        for c in s:
+            if c in ClosetoOpenMap:
+                if not sta.isempty() and sta.peek()==ClosetoOpenMap[c]:
+                    sta.pop()
+                else:
+                    return False
+            else:
+                sta.push(c)
+        
+        if sta.isempty():
+            return True 
+        else:
+            return False
